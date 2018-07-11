cedd59ff407c20d36dbde952bc6f7cbd83d3dfdf
nimac <michael12sco@yahoo.com>
2018-07-11 15:03:03 +0430

    -- making database when user doesnt have

--- src/main/java/database/DataBaseService.java
+++ src/main/java/database/DataBaseService.java
@@ -1,12 +1,14 @@
 package database;
 
+import com.mysql.cj.jdbc.exceptions.CommunicationsException;
 import models.NewsWebPageModel;
 
 import java.sql.*;
 
 public class DataBaseService implements WebSiteRepository
 {
-
+    private static final String dataBaseConfig = "useSSL=false&serverTimezone=UTC";
+    private static final String checkDataBaseExist = "CREATE DATABASE IF NOT EXISTS ";
     DataBaseService()
     {
         ConfigModel configModel = new ConfigModel("src/main/resources/config.properties");
@@ -15,29 +17,27 @@ public class DataBaseService implements WebSiteRepository
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             Connection connect = DriverManager.getConnection("jdbc:mysql://" + configModel.getHostIP() + ":" +
-                    configModel.getHostPort() + "/" + configModel.getDbName() +
-                    "?useSSL=false&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&"
-                            + "user="+ configModel.getUsername()+ "&" + "password=" + configModel.getPassword());
+                    configModel.getHostPort() +
+                    "?" + dataBaseConfig ,configModel.getUsername(), configModel.getPassword());
 
             Statement statement = connect.createStatement();
-            ResultSet resultset = statement.executeQuery("SHOW databases;");
-
-//            if (statement.execute("SHOW databases;")) {
-//                resultset = statement.getResultSet();
-//            }
+            statement.execute(checkDataBaseExist + configModel.getDbName() + ";");
+            ResultSet resultset = statement.executeQuery("show databases;");
 
             while (resultset.next()) {
                 System.out.println(resultset.getString("Database"));
             }
+
+
 
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
     }
 
+
     @Override
-    public void addWebSite(NewsWebPageModel newsWebPageModel)
-    {
+    public void addWebSite(NewsWebPageModel newsWebPageModel) {
 
     }
 }
